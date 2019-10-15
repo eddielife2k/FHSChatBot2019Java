0b023caa401352d1a0670f658c2f342dce3c17cd
Eddie Hines <44607352+eddielife2k@users.noreply.github.com>
2019-10-15 15:01:37 -0400

    Update FHSChatBot.java

--- FHSChatBot.java
+++ FHSChatBot.java
@@ -2,17 +2,23 @@
 // If it cannot answer a question, a response will allow users to refer their questions to administration.
 public class FHSChatBot
 {
-	private String name;
+  private String name;
   public FHSChatBot() {
     name = "Pat the Panther";
   }
   public String getGreeting()
 	{
-		return "Hello! I'm " + name + ". Do you have questions about FHS? If so, please tell me what it is you want to know!";
+      return "Hello! I'm " + name + ". Do you have questions about FHS? If so, please tell me what it is you want to know!";
 	}
 
 	public String getResponse(String statement)
 	{
+    if(statement.indexOf("schedule") != 0) {
+      return "Here are the schedules.";
+    }
+    if(statement.indexOf("aspen") != 0) {
+      ResponseProcessor.aspen();
+    }
     return "I don't know the answer to that question! You can contact the following people listed. [Departments with Name of Teacher] ";
 	}
 
