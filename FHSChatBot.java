89174d980a576a31794732b156b9612b1481180c
Eddie Hines <44607352+eddielife2k@users.noreply.github.com>
2019-10-22 14:50:05 -0400

    Fixed this control structure; added followup

--- FHSChatBot.java
+++ FHSChatBot.java
@@ -7,19 +7,22 @@ public class FHSChatBot
     name = "Pat the Panther";
   }
   public String getGreeting()
-	{
+    {
       return "Hello! I'm " + name + ". Do you have questions about FHS? If so, please tell me what it is you want to know!";
-	}
+    }
 
-	public String getResponse(String statement)
-	{
-    if(statement.indexOf("schedule") != 0) {
-      return "Here are the schedules.";
+    public String getResponse(String statement)
+    {
+        String whatsNext = "\n\n Do you need help with anything else?";
+        System.out.println(statement.indexOf("schedule"));
+        if(statement.indexOf("schedule") >= 0) {
+            return "Here are the schedules.";
+        }
+        else if(statement.indexOf("aspen") >= 0) {
+            return(ResponseProcessor.aspen() + whatsNext);
+        }
+        return "I don't know the answer to that question! You can contact the following people listed. [Departments with Name of Teacher] "
+        + whatsNext;
     }
-    if(statement.indexOf("aspen") != 0) {
-      ResponseProcessor.aspen();
-    }
-    return "I don't know the answer to that question! You can contact the following people listed. [Departments with Name of Teacher] ";
-	}
 
 }
